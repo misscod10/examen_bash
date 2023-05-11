08940e6bdfc23f5e316855ffeeea0eb1c8e5fd31
misscod10 <misscod10@gmail.com>
2023-05-11 20:01:09 +0200

    proba

--- santiago-script.sh
+++ santiago-script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 ################################################################
-#
 #Fes un shellscript que sigui un menú per crear usuaris, grups i afegir usuaris ja existents a grups. 
-#
-#
 ################################################################
 
-
+if (( $# > 0)) ; then
+    echo -e "Aquest script no accepta parámetres."
+    exit 2
+fi
 while true
 do
     echo -e "1)Crear usuari\n2)Crear Grup\n3)Afegir usuari al grup\n4)Sortir"
@@ -21,10 +21,11 @@ do
         read -p "Nom del usuari a agregar al grup? " USUARI_GRUP
         read -p "Nom del grup al que agregar l'usuari? " GRUP_USUARI
         sudo usermod -aG $GRUP_USUARI $USUARI_GRUP
+        echo -e "usuari $USUARI_GRUP agregat al grup $GRUP_USUARI"
     elif [[ $OPCIO -eq 4 ]] ; then
-        echo "¡Grácies per utilitzar el meu script!"
+        echo -e "¡Grácies per utilitzar el meu script, adeu!\n"
         exit 0
     else  
-        echo -e "Número equivocat\nHas d'introduïr el número identificant de alguna de les opcións..."        
+        echo -e "Ninguna opció seleccionada!\nHas d'introduïr el número identificant de alguna de les opcións..."        
     fi
 done
